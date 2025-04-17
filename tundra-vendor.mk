6b6df91b40ece8a812acf0b5eb70e628cea93c29
HotaruOs <arroztlg@gmail.com>
2025-04-17 13:58:50 -0300

    tundra: Update Audio FX modules from Oneplus 9RT
    Signed-off-by: tilt-rgb <lesma_chan@pm.me>
    Signed-off-by: HotaruOs <arroztlg@gmail.com>

--- tundra-vendor.mk
+++ tundra-vendor.mk
@@ -3143,6 +3143,7 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/tundra/proprietary/system_ext/etc/dpm/dpm.conf:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/dpm/dpm.conf \
     vendor/motorola/tundra/proprietary/system_ext/etc/init/dpmd.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/dpmd.rc \
     vendor/motorola/tundra/proprietary/system_ext/etc/init/qspmsvc.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/qspmsvc.rc \
+    vendor/motorola/tundra/proprietary/system_ext/etc/permissions/audiosphere.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/audiosphere.xml \
     vendor/motorola/tundra/proprietary/system_ext/etc/permissions/com.android.hotwordenrollment.common.util.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/com.android.hotwordenrollment.common.util.xml \
     vendor/motorola/tundra/proprietary/system_ext/etc/permissions/com.qti.dpmframework.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/com.qti.dpmframework.xml \
     vendor/motorola/tundra/proprietary/system_ext/etc/permissions/dpmapi.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/dpmapi.xml \
@@ -3670,8 +3671,6 @@ PRODUCT_PACKAGES += \
     libqcodec2_utils \
     libqcodec2_v4l2codec \
     libsmwrapper \
-    libmmieffectswrapper \
-    libspeakerbundle \
     com.mot.eeprom.mot_gt24p128e_ov50a_eeprom \
     com.mot.eeprom.mot_gt24p64e_hi1336_eeprom \
     com.mot.eeprom.mot_gt24p64e_s5kgd2_eeprom \
@@ -4232,6 +4231,7 @@ PRODUCT_PACKAGES += \
     uimgbalibrary \
     uimgbamanagerlibrary \
     uimservicelibrary \
+    audiosphere \
     com.android.hotwordenrollment.common.util \
     com.qti.dpmframework \
     dpmapi \
